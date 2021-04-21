f4f68f182a9277feffa57f257318db2b11a3351d
Andrew Broughton <a.broughton@me.com>
2021-04-21 17:11:25 -0400

    Added clear tasks and filter functionality

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function loadEventListeners() {
     form.addEventListener('submit', addTask);
     // remove task events
     taskList.addEventListener('click', removeTask) 
+    // clear task
+    clearBtn.addEventListener('click', clearTasks)
+    // filter tasks
+    filter.addEventListener('keyup', filterTasks)
 }
 
 //add task
@@ -45,10 +49,40 @@ function addTask(e) {
     e.preventDefault();
 }
 
+// remove individual task
 function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-item')) {
-        e.target.parentElement.parentElement.remove();
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
         // test
         console.log(e.target);
     }
+}
+
+// clear all task
+function clearTasks() {
+    taskList.innerHTML = '';
+    
+    // test
+    console.log('It Worked!');
+}
+
+// filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    // querySelectorAll returns a node list
+    document.querySelectorAll('.collection-item').forEach
+        (function(task){
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) !== -1) {
+                task.style.display = 'block';
+            }
+            else {
+                task.style.display = 'none';
+            }
+    });
+    // test
+    console.log(e.target.value);
 }
